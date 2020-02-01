0ad6578db5943278a6bf3219bc1ffe768dbf890e
Kael <kaelanreece@gmail.com>
2020-02-01 12:04:58 +1000

    add hello world component

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './Components/HelloWorld';
 
 const App: React.FC = () => {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>My name is Kaelan Reece</p>
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
+		<>
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>My name is Kaelan Reece</p>
+					<p>
+						Edit <code>src/App.tsx</code> and save to reload.
 				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
 				</a>
-			</header>
-		</div>
+				</header>
+				<HelloWorld />
+			</div>
+		</>
 	);
 }
 
