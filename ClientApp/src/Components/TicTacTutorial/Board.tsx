6128c27b75be090f72bbe5eec8a806518212c777
Kael <kaelanreece@gmail.com>
2020-02-02 12:08:18 +1000

    completed tic tac toe game

--- ClientApp/src/Components/TicTacTutorial/Board.tsx
+++ ClientApp/src/Components/TicTacTutorial/Board.tsx
@@ -3,19 +3,27 @@ import { Square } from './Square'
 
 interface IState {
 	squares: any[]
+	xIsNext: boolean
 }
 export default class Board extends Component<{}, IState> {
 	constructor(props) {
 		super(props);
 		this.state = {
 			squares: Array(9).fill(null),
+			xIsNext: true,
 		};
 	}
 
 	handleClick(i) {
 		const squares = this.state.squares.slice();
-		squares[i] = 'X';
-		this.setState({ squares: squares });
+		if (this.calculateWinner(squares) || squares[i]) {
+			return;
+		}
+		squares[i] = this.whoNext();
+		this.setState({
+			squares: squares,
+			xIsNext: !this.state.xIsNext,
+		});
 	}
 
 	renderSquare(i) {
@@ -27,8 +35,39 @@ export default class Board extends Component<{}, IState> {
 		)
 	}
 
+	whoNext() {
+		return this.state.xIsNext ? 'X' : 'O';
+	}
+
+	calculateWinner(squares) {
+		const lines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+		for (let i = 0; i < lines.length; i++) {
+			const [a, b, c] = lines[i];
+			if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+				return squares[a];
+			}
+		}
+		return null;
+	}
+
 	render() {
-		const status = 'Next player: X';
+		const winner = this.calculateWinner(this.state.squares);
+		let status;
+		if (winner) {
+			status = `Winner: ${winner}`;
+		} else {
+			status = `Next player: ${this.whoNext()}`;
+		}
+
 		return (
 			<div>
 				<div className="status">{status}</div>
