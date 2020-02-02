3c9a664661ba97bd32c1eadfdc8e039f96d908cb
Kael <kaelanreece@gmail.com>
2020-02-02 14:26:15 +1000

    finished tic tac toe tutorial. Moved components from previous tutorial

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HelloWorld from './Components/HelloWorld';
-import HelloUser from './Components/HelloUser';
-import Ship from './Components/Ship';
-import FavouriteColor from './Components/FavouriteColor';
-import Container from './Components/UnmountedComponent';
-import MyForm from './Components/FormExample';
-import MultipleInputForm from './Components/MultipleInputForm';
+import HelloWorld from './Components/W3-Components/HelloWorld';
+import HelloUser from './Components/W3-Components/HelloUser';
+import Ship from './Components/W3-Components/Ship';
+import FavouriteColor from './Components/W3-Components/FavouriteColor';
+import Container from './Components/W3-Components/UnmountedComponent';
+import MyForm from './Components/W3-Components/FormExample';
+import MultipleInputForm from './Components/W3-Components/MultipleInputForm';
 import Game from './Components/TicTacTutorial/Game';
 
 const App: React.FC = () => {
--- ClientApp/src/Components/TicTacTutorial/Board.tsx
+++ ClientApp/src/Components/TicTacTutorial/Board.tsx
@@ -1,76 +1,24 @@
 import React, { Component } from 'react'
 import { Square } from './Square'
-
-interface IState {
+interface IProps {
+	onClick: Function,
 	squares: any[]
-	xIsNext: boolean
 }
-export default class Board extends Component<{}, IState> {
-	constructor(props) {
-		super(props);
-		this.state = {
-			squares: Array(9).fill(null),
-			xIsNext: true,
-		};
-	}
+export default class Board extends Component<IProps> {
 
-	handleClick(i) {
-		const squares = this.state.squares.slice();
-		if (this.calculateWinner(squares) || squares[i]) {
-			return;
-		}
-		squares[i] = this.whoNext();
-		this.setState({
-			squares: squares,
-			xIsNext: !this.state.xIsNext,
-		});
-	}
 
 	renderSquare(i) {
 		return (
 			<Square
-				value={this.state.squares[i]}
-				onClick={() => this.handleClick(i)}
+				value={this.props.squares[i]}
+				onClick={() => this.props.onClick(i)}
 			/>
 		)
 	}
 
-	whoNext() {
-		return this.state.xIsNext ? 'X' : 'O';
-	}
-
-	calculateWinner(squares) {
-		const lines = [
-			[0, 1, 2],
-			[3, 4, 5],
-			[6, 7, 8],
-			[0, 3, 6],
-			[1, 4, 7],
-			[2, 5, 8],
-			[0, 4, 8],
-			[2, 4, 6],
-		];
-		for (let i = 0; i < lines.length; i++) {
-			const [a, b, c] = lines[i];
-			if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-				return squares[a];
-			}
-		}
-		return null;
-	}
-
 	render() {
-		const winner = this.calculateWinner(this.state.squares);
-		let status;
-		if (winner) {
-			status = `Winner: ${winner}`;
-		} else {
-			status = `Next player: ${this.whoNext()}`;
-		}
-
 		return (
 			<div>
-				<div className="status">{status}</div>
 				<div className="board-row">
 					{this.renderSquare(0)}
 					{this.renderSquare(1)}
